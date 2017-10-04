26803ddb51a4bd023eba04196d083266018a5a4f
Andy MacGregor <Andrew_MacGregor@student.uml.edu>
2017-10-04 14:40:19 -0400

    Removed the helper function I wrote, because why reinvent whats there?

--- TestHelpers.java
+++ TestHelpers.java
@@ -8,23 +8,5 @@ import java.util.Queue;
 import javax.imageio.ImageIO;
 
 public class TestHelpers {
-	public static void main(String[] args)
-	{
-		try{
-			testMakePackets();
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void testMakePackets() throws IOException
-	{
-		String imName = "JustATest.jpg";
-		File imFile = new File(imName);
-		BufferedImage im = ImageIO.read(imFile);
-		
-		
-		Queue<byte[]> q = HelperFunctions.makePacket(im, 1024, "jpg");
-		System.out.println("Make breakpoint here and look at q");
-	}
+	//TODO tests if we ever make them
 }
