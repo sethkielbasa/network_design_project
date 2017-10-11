1ed1c605ccb4ef774bfa4ac9b5493fa997cc75a7
skielbasa <skielbas@umass.edu>
2017-10-10 23:16:18 -0400

    Major changes
    refactored some of the code

--- UDPClient.java
+++ UDPClient.java
@@ -4,7 +4,9 @@ import java.io.*;
 import java.net.*;
 public class UDPClient implements Runnable{
 	
-	public static final int BYTES_PER_PACKET = 1024; //set packet size
+	private final int DATA_SIZE = 1024; //set packet size
+	private final int HEADER_SIZE = 6;
+	private final int PACKET_SIZE = DATA_SIZE + HEADER_SIZE;
 	String imageName;
 	int port;
 	boolean packetLogging;
@@ -37,6 +39,7 @@ public class UDPClient implements Runnable{
 			}
 		}
 	}
+	
 	public UDPClient(String image, int portNum, boolean logging)
 	{
 		port = portNum;
@@ -53,21 +56,72 @@ public class UDPClient implements Runnable{
 		}
 	}
 	
-	public static int getNumberOfPacketsToSend(String file_to_send) throws IOException{
+	private int getNumberOfPacketsToSend(String file_to_send) throws IOException{
 		int number_of_packets = 0;
 		System.out.println(file_to_send);
 		FileInputStream fis = new FileInputStream( file_to_send ); //Open file to send
-		number_of_packets = (fis.available() / BYTES_PER_PACKET); //size of file divided by packet size
-		if ( fis.available() % BYTES_PER_PACKET > 0){ //if there are bytes leftover
+		number_of_packets = (fis.available() / DATA_SIZE); //size of file divided by packet size
+		if ( fis.available() % DATA_SIZE > 0){ //if there are bytes leftover
 			number_of_packets++; 
 		}
 		fis.close();
 		return number_of_packets;
 	}
-	
+
+/*
 	public static void main(String args[]) throws Exception {
 		UDPClient c = new UDPClient("client_image.jpg", 9878, false);
 		c.transferImage();
+	}
+*/	
+	
+	private void transmitPacket(byte[] packet, DatagramSocket socket ) throws Exception{
+		
+		InetAddress IPAddress = InetAddress.getByName("localhost");		
+		DatagramPacket sendPacket = new DatagramPacket(packet, packet.length, IPAddress, port);
+		socket.send(sendPacket);
+		
+	}
+	
+	private byte[] addPacketHeader(int packetSize, byte[] readData ){
+		
+		byte[] packet = new byte[packetSize + HEADER_SIZE];
+		
+		for ( int i = 0; i < packetSize; i++){
+			packet[i + HEADER_SIZE] = readData[i];
+		}
+		
+		byte[] checksum = new byte[2];
+		checksum = calculateChecksum( readData );
+		packet[2] = checksum[0];
+		packet[3] = checksum[1];
+		
+		byte[] ackNumber = new byte[2];
+		ackNumber = calculateAckNumber();
+		packet[0] = ackNumber[0];
+		packet[1] = ackNumber[1];
+		
+		assert ( packetSize > 1024 );
+		packet[5] = (byte) (packetSize & 0xFF);
+		packet[4] = (byte) ((packetSize >> 8) & 0xFF);
+		
+		return packet;
+	}
+	
+	private byte[] calculateChecksum( byte[] readData ){
+		byte[] checksum = new byte[2];
+		checksum[0] = 0;
+		checksum[1] = 0;
+		
+		return checksum;
+	}
+	
+	private byte[] calculateAckNumber(){
+		byte[] ackNum = new byte[2];
+		ackNum[0] = 0;
+		ackNum[1] = 0;
+		
+		return ackNum;
 	}
 	
 	public void transferImage() throws Exception
@@ -78,44 +132,58 @@ public class UDPClient implements Runnable{
 		 *  https://lowell.umassonline.net/bbcswebdav/pid-305360-dt-content-rid-977475_1/courses/L2710-11029/Sockets.pdf
 		 * 
 		 */
+		
 		//Socket setup 
-		DatagramSocket clientSocket = new DatagramSocket();
-		InetAddress IPAddress = InetAddress.getByName("localhost");
-		byte[] sendSize;
-		byte[] receiveData = new byte[BYTES_PER_PACKET];
-		int num_packets = getNumberOfPacketsToSend( imageName ); //get number of packets
 		
-		log( "CLIENT: Sending " + num_packets + " packets");
+		DatagramSocket clientSocket = new DatagramSocket();	
+		byte[] packet;
 		
+		
+				
 		//Send amount packets to expect to the server
-		sendSize = new byte[String.valueOf(num_packets).getBytes().length];
-		sendSize = String.valueOf(num_packets).getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(sendSize, sendSize.length, IPAddress, port);
-		clientSocket.send(sendPacket);
+		int num_packets = getNumberOfPacketsToSend( imageName ); //get number of packets
+		log( "CLIENT: Going to send " + num_packets + " packets");
+		
+		
+		int packet_length = String.valueOf(num_packets).getBytes().length;
+		packet = new byte[packet_length];
+		packet = String.valueOf(num_packets).getBytes();
+		packet = addPacketHeader( packet_length, packet );
+		transmitPacket(packet, clientSocket);
+				
 		
 		while(true){
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			packet = new byte[PACKET_SIZE];
+			DatagramPacket receivePacket = new DatagramPacket(packet, packet.length);
 			clientSocket.receive(receivePacket);
 			break;
 		}
 		
+		
 		FileInputStream fis = new FileInputStream( imageName );		
+		log( "CLIENT: Sending packets");
+		
 		while( true ){
 			int data_size = fis.available(); //get bytes left to read
-			if (data_size > 1024){
-				data_size = 1024; //max 1024 at a time
+			if (data_size > DATA_SIZE){
+				data_size = DATA_SIZE; //max 1024 at a time
 			}
 			else if (data_size == 0){
 				break;
 			}
-			byte[] sendData = new byte[data_size]; // create buffer for data
-			int flag = fis.read(sendData); //read data
+			
+			byte[] readData = new byte[data_size];
+			
+			int flag = fis.read(readData); //read data
 			if (flag == -1){ //if end of file is reached
 				break;
 			}
-			sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-			clientSocket.send(sendPacket);
+			//System.out.println(data_size);
+			packet = new byte[data_size + HEADER_SIZE];
+			packet = addPacketHeader( data_size, readData );
+			transmitPacket(packet, clientSocket);
 		}
+		
 		
 		fis.close();
 		
@@ -126,20 +194,17 @@ public class UDPClient implements Runnable{
 		 * 
 		 */
 		log("CLIENT: Waiting for server response");
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+		
+		packet = new byte[PACKET_SIZE];
+		DatagramPacket receivePacket = new DatagramPacket(packet, packet.length);
 		clientSocket.receive(receivePacket);
+		
 		String serverResponse = new String(receivePacket.getData());
 		
-		int substring = 0;
-		for( int i = 0; i<1024; i++)
-		{
-			if ( Character.isDigit(serverResponse.charAt(i)) )
-				substring++;
-			else
-				break;
-		}	
-		
-		log("FROM SERVER: " + serverResponse.substring(0, substring ) + " packets received");
+		int packetLength = packet[4] & 0xFF;
+		packetLength = packetLength << 8;
+		packetLength = packetLength | (packet [5] & 0xFF);
+		log("FROM SERVER: " + serverResponse.substring( HEADER_SIZE , HEADER_SIZE + packetLength ));
 		
 		//Close the log
 		if(packetLogging)
